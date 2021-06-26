b6317b1d870191ada7657e94d9210864fe9a13c3
Stephen Baione <sbaione@email.sc.edu>
2021-06-26 19:03:58 -0400

    Removed test directory

--- persona/routers/test.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-from service import internal
-
-@router.get("/users/", tags=["users"])
-async def read_users():
-    return {"username": "test"}
