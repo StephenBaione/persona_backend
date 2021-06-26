52068748a36bda27f80c5258a23d5de037011db2
Stephen Baione <sbaione@email.sc.edu>
2021-06-26 19:24:50 -0400

    Implemented Token flow for login verification

---
+++ persona/core/security/auth/user_auth.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+from fastapi import FastAPI, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+from core.data.models.auth.token import Token
+from core.data.models.user.model import User
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def verify_password(text_password: str, hashed_password: str):
+    return pwd_context.verify(text_password, hashed_password)
+
+def get_password_hash(text_password: str):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def authenticate_user(username: str, password: str, user: User):
+    if not verify_password(password, user.password_hash):
+        return False
+    return True
